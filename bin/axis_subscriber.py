80b5a4f688948ecca8a91332167c60168416dca2
vschmidt <vschmidt@ccom.unh.edu>
2016-03-17 12:04:41 -0400

    Added argument parsing for the axis subscriber and the abilty to write to a file (with a copy of latest.jpg maintained).

--- bin/axis_subscriber.py
+++ bin/axis_subscriber.py
@@ -10,26 +10,57 @@ import dataproxy
 import time
 import datetime as dt
 import sys
+import os
+from shutil import copyfile
 #import Image
 #from cStringIO import StringIO
 #import Tk, ImageTk
+
+# Set up argument parsing
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-f","--forwarder",
+                    action = "store",
+                    default = "localhost",
+                    help = "IP address or DNS name of the forwarder")
+parser.add_argument("-i","--topicID",
+                    action = "store",
+                    default = "TEST_123",
+                    help = "Text string for ID of this published topic.")
+parser.add_argument("-d","--log_dir",
+                    action = "store",
+                    default = ".",
+                    help = "Log images to directory")
+parser.add_argument("-v","--verbosity",
+                    action = "count",
+                    help = "Verbose output to terminal")
+
+global log_dir
+log_dir = "."
 
 class subscriber(dataproxy.subscriber):
 
     def process_data(self,data):
         # The axis camera will send jpg images. For now we simply capture them and write them to disk.
         #im = Image(StringIO(data.read()))
-        fh = open('image_%02d.jpg' % dt.datetime.now().second,'w')
+        global log_dir
+        
+        #filename = os.path.join(log_dir,'image_%02d.jpg' % dt.datetime.now().second)
+        filename = os.path.join(log_dir,'image_%d.jpg' % int(time.time()))
+        fh = open(filename,'w')
         fh.write(data)
         fh.close()
+        copyfile(filename,os.path.join(log_dir,'latest.jpg'))
         return
 
 if __name__ == "__main__":
-    
-    if sys.argv.__len__() > 1:
-        forwarder = sys.argv[1]
-    else:
-        forwarder = "localhost"
 
-    s = subscriber(forwarderIP = forwarder,topicfilter = "CAMERA_01")
+    # Handle arguments
+    args = parser.parse_args()
+    forwarder = args.forwarder
+    topicID = args.topicID
+    log_dir = args.log_dir
+    verbosity = args.verbosity
+
+    s = subscriber(forwarderIP = forwarder,topicfilter = topicID)
     s.run()
